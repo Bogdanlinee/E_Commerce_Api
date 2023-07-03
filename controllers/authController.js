0247f1f5fb1b3cdeb5836afe6fa53f1984a13766
Dan Berezin <Bogdanlinee@gmail.com>
2023-07-03 21:11:56 +0300

    Changed controllers a bit added pass encription

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,29 @@
 const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors/index');
 
 const register = async (req, res) => {
-  const { name, email, password, role } = req.body;
-  const userData = { name, email, password, };
-  if (role) {
-    userData.role = role;
+  const { name, email, password } = req.body;
+  const userData = { name, email, password };
+  const isEmailAlreadyExist = await User.findOne({ email });
+
+  if (isEmailAlreadyExist) {
+    throw new CustomError.BadRequestError('Email is already exists');
   }
 
+  const isTheFirstUser = await User.countDocuments({}) === 0;
+  const role = isTheFirstUser ? 'admin' : 'user';
+
+  userData.role = role;
+
   const user = await User.create(userData);
-  console.log(user);
-  res.json(user);
+  res.status(StatusCodes.CREATED).json({ user });
 }
 const login = async (req, res) => {
   res.send('login')
 }
 const logout = async (req, res) => {
-  res.send('logoutsome string value')
+  res.send('logout some string value')
 }
 
 module.exports = { register, login, logout }
