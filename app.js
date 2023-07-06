a2a18ada847b97b08d5f5390ddab49e3ac01abd2
Dan Berezin <Bogdanlinee@gmail.com>
2023-07-06 21:12:39 +0300

    Logout

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 // router export
 const authRouter = require('./routes/authRoutes');
@@ -23,6 +24,7 @@ app.use('/api/v1/auth', authRouter)
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
